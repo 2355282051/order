872ca568bcf80600067d0c4f3794992fd3d65594
dancingcorp <dancingcorp@163.com>
2015-01-26 20:16:46 +0800

    获取员工列表等,超时返回空结果

--- src/main/java/com/boka/user/controller/DesignerController.java
+++ src/main/java/com/boka/user/controller/DesignerController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -139,6 +140,7 @@ public class DesignerController {
 //            deviceId = map.get("deviceId");
 //            userId = map.get("userId");
             result.setResult(designerService.getShopDesigner(id));
+        } catch (IOException ie) {
         } catch (Exception e) {
             result.setCode(500);
             result.setSuccess(false);
