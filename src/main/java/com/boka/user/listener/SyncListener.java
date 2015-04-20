86e6cb250c9494929c5a886e9a48d25cec4f71cd
Fu Zhaohui <fuzhaohui200@gmail.com>
2015-04-20 18:41:23 +0800

    1. modify return

--- src/main/java/com/boka/user/listener/SyncListener.java
+++ src/main/java/com/boka/user/listener/SyncListener.java
@@ -2,6 +2,7 @@ package com.boka.user.listener;
 
 import com.alibaba.fastjson.JSON;
 import com.aliyun.openservices.ons.api.*;
+import com.boka.user.constant.SystemConstant;
 import com.boka.user.model.Designer;
 import com.boka.user.service.DesignerService;
 import org.springframework.web.context.WebApplicationContext;
@@ -17,7 +18,9 @@ import java.util.Properties;
 public class SyncListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        syncDesigner(servletContextEvent);
+        if(SystemConstant.ONS_STARTED) {
+            syncDesigner(servletContextEvent);
+        }
     }
 
     @Override
