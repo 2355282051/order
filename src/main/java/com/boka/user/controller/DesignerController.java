1ea855990d72938459ee14e35a43fc279786c667
dancingcorp <dancingcorp@163.com>
2015-04-21 13:51:27 +0800

    正式环境提交

--- src/main/java/com/boka/user/controller/DesignerController.java
+++ src/main/java/com/boka/user/controller/DesignerController.java
@@ -5,16 +5,19 @@ import com.boka.common.constant.ServiceType;
 import com.boka.common.dto.ResultTO;
 import com.boka.common.exception.AuthException;
 import com.boka.common.exception.CommonException;
+import com.boka.common.util.Assert;
 import com.boka.common.util.AuthUtil;
 import com.boka.common.util.LogUtil;
 import com.boka.user.model.Location;
 import com.boka.user.service.DesignerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
@@ -26,6 +29,9 @@ public class DesignerController {
 
     @Autowired
     private AuthUtil authUtil;
+
+    @Resource(name = "redisTemplate")
+    private HashOperations<String, String, String> hashOps;
 
     @RequestMapping(value = "/designer/near/c/{city}", method = RequestMethod.GET)
     public ResultTO getNearDesigners(HttpServletRequest request, Double lat, Double lng, int page, @PathVariable("city") String city, String keyword) {
@@ -97,9 +103,12 @@ public class DesignerController {
     public ResultTO getUserInfo(HttpServletRequest request, @PathVariable("id") String id) {
         ResultTO result = new ResultTO();
         try {
-            Map<String, String> map = authUtil.preAuth(request);
-            String userId = map.get("userId");
-            result.setResult(designerService.getUserInfo(id, userId, request.getHeader("access_token"), request.getHeader("device_id")));
+            String userId = null;
+            String access_token = request.getHeader("access_token");
+            if (Assert.isNotNull(access_token)) {
+                userId = hashOps.get(access_token, "userId");
+            }
+            result.setResult(designerService.getUserInfo(id, userId, access_token, request.getHeader("device_id")));
         } catch (CommonException ce) {
             result.setCode(400);
             result.setSuccess(false);
